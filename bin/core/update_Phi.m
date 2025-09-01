f50f6010f21aa60d91f6ad2d9b600ea06e7537be
shijie321 <57472811+shijie321@users.noreply.github.com>
2025-09-01 11:37:23 +0200

    Update update_Phi.m

--- bin/core/update_Phi.m
+++ bin/core/update_Phi.m
@@ -1,4 +1,4 @@
-function Phi = update_Phi(phi, grad_phi, alphai, options)
+function phi = update_Phi(phi, grad_phi, alphai, options)
 % UPDATE_PHI updates the yaw angle control inputs using gradient descent.
 
 phi_lim = options.AMPC.Phi_lim;
@@ -6,10 +6,12 @@ Nc      = options.AMPC.Nc;
 dJ      = grad_phi'; % Transpose gradient to match phi dimensions
 
 % Simple gradient descent step
-phi = phi - dJ * alphai;
+phis = phi(:, 1:Nc) - dJ * alphai;
 
 % Apply hard constraints (clipping)
-phi(phi > phi_lim(2)) = phi_lim(2);
-phi(phi < phi_lim(1)) = phi_lim(1);
+phis(phis > phi_lim(2)) = phi_lim(2);
+phis(phis < phi_lim(1)) = phi_lim(1);
+
+phi = [phis phis(:,end) * ones(1, length(phi) - Nc)];
 
 end
