ae5da7a40a6a756a5e971e0897397d5123f842a3
shijie321 <57472811+shijie321@users.noreply.github.com>
2025-09-01 11:34:35 +0200

    Update update_beta.m

--- bin/core/update_beta.m
+++ bin/core/update_beta.m
@@ -1,31 +1,35 @@
-function beta = update_beta(beta,grad,alphai,beta0,options)
+function beta = update_beta(beta,grad,alphai,options)
 
 beta_lim  = options.AMPC.beta_lim;
 Nc        = options.AMPC.Nc;
-dbeta_max = options.AMPC.dbeta_max;
+%dbeta_max = options.AMPC.dbeta_max;
 
 % Find new beta
 dJ      = grad';
-s       = sign(dJ);
+%s       = sign(dJ);
 betas   = beta(:,1:Nc);
 %{
 betas   = betas-((beta_lim(2)-betas).*(s==-1)+...
             (betas-beta_lim(1)).*(s==1)).*dJ*alphai;%%%%% if for some betas<beta_lim(1) and s==1, it should stop update??????
 %}
-betas   = betas-dJ*alphai;       
+betas   = betas-dJ*alphai;  
+
+betas(betas > beta_lim(2)) = beta_lim(2);
+betas(betas < beta_lim(1)) = beta_lim(1);
+
 % s.t. constraint
 %if nargin > 5
 %     betas   = [betas(:,1) betas];
-    betas   = [beta0 betas];
-    for i = 1:Nc
-        dbeta           = diff(betas(:,i:i+1),1,2);
-        betas(:,i+1)    = (abs(dbeta)>dbeta_max).*(betas(:,i)+...
-                            dbeta_max.*sign(dbeta))+...
-                            (abs(dbeta)<=dbeta_max).*betas(:,i+1);
-    end
-    betas = betas(:,2:end);
+%    betas   = [beta0 betas];
+%    for i = 1:Nc
+%        dbeta           = diff(betas(:,i:i+1),1,2);
+%        betas(:,i+1)    = (abs(dbeta)>dbeta_max).*(betas(:,i)+...
+%                            dbeta_max.*sign(dbeta))+...
+%                            (abs(dbeta)<=dbeta_max).*betas(:,i+1);
+%    end
+%    betas = betas(:,2:end);
 %end
 
 beta    = [betas betas(:,end)*ones(1,length(beta)-Nc)];
-beta    = betalim(beta,beta_lim,dbeta_max);
+%beta    = betalim(beta,beta_lim,dbeta_max);
 
